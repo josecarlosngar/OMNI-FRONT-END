55aab93fbb2b45a9f097545a37c2d317bd2f52f7
rsPablo <pablors92@gmail.com>
2018-05-25 13:52:19 +0200

    Bootstrap añadido

---
+++ src/app/vista-por-defecto/vista-por-defecto.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-vista-por-defecto',
+  templateUrl: './vista-por-defecto.component.html',
+  styleUrls: ['./vista-por-defecto.component.css']
+})
+export class VistaPorDefectoComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
