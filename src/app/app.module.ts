1559a6d689abd14632d923b9520056412af40806
rsPablo <pablors92@gmail.com>
2018-06-04 13:33:54 +0200

    VistaPorDefecto

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,12 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { VistaPorDefectoComponent } from './vista-por-defecto/vista-por-defecto.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    VistaPorDefectoComponent
   ],
   imports: [
     BrowserModule
